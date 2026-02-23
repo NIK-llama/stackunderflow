8241d1916451bf005d10fd8286337310924e5712
NIK-llama <nikshitog@gmail.com>
2026-02-23 23:46:08 +0530

    config github O-Auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,27 @@
-export default function Home() {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
   return (
-    <div>
-    </div>
+    <>
+      <h1 className="h1-bold">Welcome to StackUnderFlow</h1>
+
+      <form
+        className="px-10 pt-25"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
   );
-}
+};
+
+export default Home;
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,11 +1,34 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "../ui/button";
+import { toast } from "sonner";
+import { signIn } from "next-auth/react";
+import ROUTES from "@/constants/routes";
 
 const SocialAuthForm = () => {
-  const buttonClass = "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
+  const buttonClass =
+    "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
+
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+      });
+    } catch (error) {
+      console.log(error);
+
+      toast.error("Sign-in Failed", {
+        description:
+          error instanceof Error
+            ? error.message
+            : "An unexpected error occurred.",
+      });
+    }
+  };
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("github")}>
         <Image
           src="/icons/github.svg"
           alt="Github Logo"
@@ -16,7 +39,7 @@ const SocialAuthForm = () => {
         <span>Log in with GitHub</span>
       </Button>
 
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
         <Image
           src="/icons/google.svg"
           alt="Google Logo"
