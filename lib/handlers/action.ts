0e0db3b7ee07087ed8eb689bc5c12e68646062f4
NIK-llama <nikshitog@gmail.com>
2026-04-08 03:08:03 +0530

    implemented action handler

---
+++ lib/handlers/action.ts
@@ -0,0 +1,44 @@
+import { ZodError, ZodType } from "zod";
+import { UnauthorizedError, ValidationError } from "../http-errors";
+import { Session } from "next-auth";
+import { auth } from "@/auth";
+
+type ActionOptions<T> = {
+  params?: T;
+  schema?: ZodType<T>;
+  authorize?: boolean;
+};
+
+async function action<T>({
+  params,
+  schema,
+  authorize = false,
+}: ActionOptions<T>) {
+  if (schema && params) {
+    try {
+      schema.parse(params);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return new ValidationError(
+          error.flatten().fieldErrors as Record<string, string[]>,
+        );
+      } else {
+        return new Error("Schema validation failed");
+      }
+    }
+  }
+
+  let session: Session | null = null;
+
+  if(authorize) {
+    session = await auth();
+
+    if(!session) {
+        return new UnauthorizedError();
+    }
+  }
+
+  return { params, session };
+}
+
+export default action;
