bdf5d479b0fcc23ed5530e27b890feabbc9b950c
NIK-llama <nikshitog@gmail.com>
2026-04-11 01:06:50 +0530

    implemented sign-in action

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -3,10 +3,11 @@
 import { ActionResponse, ErrorResponse } from "@/types/global";
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { SignUpSchema } from "../validations";
+import { SignInSchema, SignUpSchema } from "../validations";
 import prisma from "../prisma";
 import bcrypt from "bcryptjs";
 import { signIn } from "@/auth";
+import { NotFoundError } from "../http-errors";
 
 export async function signUpWithCredentials(
   params: AuthCredentials,
@@ -51,6 +52,52 @@ export async function signUpWithCredentials(
         },
       });
     });
+
+    await signIn("credentials", { email, password, redirect: false });
+
+    return { success: true };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function signInWithCredentials(
+  params: Pick<AuthCredentials, "email" | "password">,
+): Promise<ActionResponse> {
+  const validationResult = await action({ params, schema: SignInSchema });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { email, password } = validationResult.params!;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+      include: {
+        accounts: {
+          where: {
+            provider: "credentials",
+          },
+        },
+      },
+    });
+
+    if (!user) throw new NotFoundError("User");
+
+    const credentialsAccount = user.accounts[0];
+
+    if (!credentialsAccount || !credentialsAccount.password) {
+      throw new Error("Invalid login method or account not found");
+    }
+
+    const passwordMatch = await bcrypt.compare(
+      password,
+      credentialsAccount.password,
+    );
+
+    if (!passwordMatch) throw new Error("Password does not match");
 
     await signIn("credentials", { email, password, redirect: false });
 
